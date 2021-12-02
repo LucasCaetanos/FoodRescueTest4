2da7b470959a2768deaaec2f721b0261c361c325
Lucas Caetano <87659302+LucasCaetanos@users.noreply.github.com>
2021-12-02 15:56:57 -0300

    Delete FoodRescueTest3-master directory

--- FoodRescueTest3-master/app/src/main/java/com/batista/foodrescue/TaskPlannerApplication.kt
+++
@@ -1,7 +0,0 @@
-package com.batista.foodrescue
-
-import android.app.Application
-import dagger.hilt.android.HiltAndroidApp
-
-@HiltAndroidApp
-class TaskPlannerApplication : Application()
--- FoodRescueTest3-master/app/src/main/java/com/batista/foodrescue/views/list/adapter/ProdutoClickListener.kt
+++
@@ -1,8 +0,0 @@
-package com.batista.foodrescue.views.list.adapter
-
-import com.batista.foodrescue.data.model.Produto
-
-interface ProdutoClickListener {
-
-    fun onTaskClicked(produto: Produto)
-}
